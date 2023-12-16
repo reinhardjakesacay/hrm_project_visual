d47deec62d4e042c7dbc5a05bae27b72603dd12f
reinhardjakesacay <reinhardjakesacayforwork@gmail.com>
2023-12-16 13:12:10 +0800

    home_page style

--- home_page.php
+++ home_page.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>WorkEffecient</title>
 </head>
 <body>
-    <h1>Employee skill competencies and perception of SDE</h1>
-    <button><a href="skills.php">Skill</a></button>
-    <button><a href="job_position.php">Job Position</a></button>
-    <button><a href="address.php">Residence</a></button>
+    <div class="container">
+        <h1>Employee Skill Competencies and <br>Perception of SDE</h1>
+            <div class="nav">
+                <button><a href="skills.php">Skill</a></button>
+                <button><a href="job_position.php">Job Position</a></button>
+                <button><a href="address.php">Residence</a></button>
+            </div>
+    </div>
 </body>
 </html>
